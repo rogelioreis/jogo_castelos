c638411eb8168fe5b0bcb5e7e7ca3c66fc120a5c
Rogelio Reis <150390942+rogelioreis@users.noreply.github.com>
2024-10-18 20:57:37 -0300

    Retirar Comentário Desnecessário
    Comentário do trabalho

--- jogo/jogo_teste/jogo_castelos/JogoMain.java
+++ jogo/jogo_teste/jogo_castelos/JogoMain.java
@@ -1,25 +1,5 @@
 package jogo_castelos;
 
-//	2) Implementar um aplicativo que crie 2 castelos japoneses (com 15 samurais, 2 vidas e 2
-//	defesas) e 2 europeus (com 10 paladinos, 2 vidas e 3 defesas), armazene-os em uma
-//	lista.
-
-//	Jogar até que 3 castelos sejam destruídos.
-//	O mecanismo do jogo é:
-//	1. O primeiro castelo da lista (por exemplo C1) escolhe outro castelo (Por exemplo C2)
-//	para atacar (você pode pedir para o usuário ou escolher de modo aleatório)
-//	2. Ele sorteia um número de 0 a 10. Se tirar zero, ele passa a vez. Caso contrário, C1
-//	destrói essa quantidade de guerreiros de C2. Em seguida, mostra na tela a
-//	situação do castelo C2.
-//	3. Agora é a vez do segundo castelo jogar. Ele deve escolher outro castelo para atacar
-//	e voltar ao passo 2.
-//	4. Após o último castelo da lista jogar, a vez volta para o primeiro castelo.
-
-//	Quando um castelo for completamente destruído, deve mostrar na tela a situação dele e a
-//	mensagem “CASTELO ___ DESTRUÍDO”, então ele é retirado da lista. O jogo acaba
-//	quando sobrar apenas um castelo na lista, que será o vencedor e mostrará na tela
-//	“CASTELO ___ VENCEDOR”
-
 import java.util.LinkedList;
 
 public class JogoMain {
@@ -27,7 +7,8 @@ public class JogoMain {
 	public static void main(String[] args) {
 		
 		LinkedList<Castelo> vet = new LinkedList<Castelo>();
-		
+
+		// Criando Castelos
 		Japoneses j1 = new Japoneses(2, 2, "Japones 1", 15);
 		Japoneses j2 = new Japoneses(2, 2, "Japones 2", 15);
 		Europeu e1 = new Europeu(3,2, "Europeu 1", 10);
